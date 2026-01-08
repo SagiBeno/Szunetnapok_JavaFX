63a5192680ce805ae1ad44f083f7f2c2f59f7ee5
SagiBeno <benosagi321@gmail.com>
2026-01-08 08:09:59 +0100

    Import modules

--- src/main/java/com/example/szunetnapok/SzunetnapokController.java
+++ src/main/java/com/example/szunetnapok/SzunetnapokController.java
@@ -3,6 +3,18 @@ package com.example.szunetnapok;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
+import java.io.InputStream;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
+import org.xml.sax.InputSource;
+
+import java.io.IOException;
+
+
 public class SzunetnapokController {
     @FXML
     private Label welcomeText;
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,8 @@
 module com.example.szunetnapok {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.net.http;
+    requires java.xml;
 
 
     opens com.example.szunetnapok to javafx.fxml;
