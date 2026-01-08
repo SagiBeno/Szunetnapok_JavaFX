70a1ca60f4be8082546895f6fc803e3aa29ea1f9
SagiBeno <benosagi321@gmail.com>
2026-01-08 09:11:19 +0100

    Show year options

--- src/main/java/com/example/szunetnapok/SzunetnapokController.java
+++ src/main/java/com/example/szunetnapok/SzunetnapokController.java
@@ -1,17 +1,28 @@
 package com.example.szunetnapok;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Spinner;
+import javafx.scene.control.SpinnerValueFactory;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.Date;
+import java.util.ResourceBundle;
 
 
-public class SzunetnapokController {
-    @FXML
-    private Label welcomeText;
+public class SzunetnapokController implements Initializable {
 
-    @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    @FXML public Spinner<Integer> spinner_year;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        SpinnerValueFactory.IntegerSpinnerValueFactory integerSpinnerValueFactory = new SpinnerValueFactory.IntegerSpinnerValueFactory(2025, 2030);
+        spinner_year.setValueFactory(integerSpinnerValueFactory);
+        spinner_year.getValueFactory().setValue(2026);
+    }
+
+    @FXML protected void onShowButtonClick() throws IOException, InterruptedException {
+        YearHolidays yearHolidays = SzunetNapokApiClient.getYear(spinner_year.getValueFactory().getValue());
     }
 }
